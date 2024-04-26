851e7da48262c0cd46369a288e2edf621830d1b6
Tia i X570-ACE <hong@ithuan.tw>
2024-04-26 12:20:37 +0800

    全部16族。

--- download_odt.sh
+++ download_odt.sh
@@ -5,7 +5,7 @@ set -eu
 directionay="odt"
 mkdir -p "${directionay}"
 
-for lang in ami trv
+for lang in ami tay pwn bnn pyu dru tsu xsy tao ssf ckv trv ais sdq sxr xnb
 do
 	wget "https://e-dictionary.ilrdf.org.tw/${lang}/download/odt/1.htm" \
 		-O "${directionay}/${lang}.odt"
