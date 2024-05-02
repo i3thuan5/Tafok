c6886131889603f9d2ee41e5e705540d57205203
Tia i X570-ACE <hong@ithuan.tw>
2024-05-02 10:33:45 +0800

    處理bô-kâng格式。

--- extract_audio_url.sh
+++ extract_audio_url.sh
@@ -8,12 +8,13 @@ rm -f "${result}"
 
 for file_path in `find ${directionay_no_json} -type f`
 do
-	cat json/ami/cangra.json \
-		| jq '.GenericData.DATA[].File.Path' \
+	cat "${file_path}" \
+		| jq '.GenericData.DATA[].File?.Path' \
 		>> "${result}"
-	cat json/ami/cangra.json \
-		| jq '.GenericData.DATA[].Explanation.Sentence?.File.Path' \
+	cat "${file_path}" \
+		| jq '.GenericData.DATA[].Explanation?.Sentence?.File.Path' \
 		>> "${result}"
-	cat json/ami/cangra.json | jq '.GenericData.DATA[].Explanation[]?.Sentence?.File.Path' \
+	cat "${file_path}" \
+		| jq '.GenericData.DATA[].Explanation?[]?.Sentence?.File.Path' \
 		>> "${result}"
 done
