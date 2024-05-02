f511230dad60c0e6e6e76103f3d5bb950aa8276c
Tia i X570-ACE <hong@ithuan.tw>
2024-05-02 13:42:39 +0800

    tao分開。

--- extract_audio_url.sh
+++ extract_audio_url.sh
@@ -17,4 +17,18 @@ do
 	cat "${file_path}" \
 		| jq --raw-output '.GenericData.DATA[]?.Explanation?[]?.Sentence?.File?.Path' \
 		>> "${result}"
-done <   <(find ${directionay_no_json} -type f)
+done <   <(find ${directionay_no_json} -type f ! -path 'json/tao/*')
+
+## Tao
+while IFS= read -r -d '' file_path
+do
+	cat "${file_path}" \
+		| jq --raw-output '.GenericData.DATA[]?.File?.Path'\
+		>> "${result}"
+	cat "${file_path}" \
+		| jq --raw-output '.GenericData.DATA[]?.Explanation?.Sentence?.File?.Path' \
+		>> "${result}"
+	cat "${file_path}" \
+		| jq --raw-output '.GenericData.DATA[]?.Explanation?[]?.Sentence?.File?.Path' \
+		>> "${result}"
+done <   <(find ${directionay_no_json} -type f -path 'json/tao/*')
