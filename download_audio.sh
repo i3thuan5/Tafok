ee724ac9a60cabc0c9458822b6db9f9be0738c53
Tia i X570-ACE <hong@ithuan.tw>
2024-05-02 10:50:09 +0800

    修pipe

--- download_audio.sh
+++ download_audio.sh
@@ -6,5 +6,5 @@ result="audio_list.txt"
 cat ${result} \
 	| grep -v null \
 	| sort -u \
-	| sed 's/^"\(.*\)"$/\1/g'
+	| sed 's/^"\(.*\)"$/\1/g' \
 	| wget --input-file - --no-host-directories --force-directories
